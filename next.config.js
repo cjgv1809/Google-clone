a77eadaf5e1c697cdc433eef066422fd1beab6c9
Carlos <carlos_gomes1809@hotmail.com>
2021-04-29 20:02:50 -0300

    next.config.js file added

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    domains: ["www.google.com"],
+  },
+  env: {
+    API_KEY: "AIzaSyC60JUnJpZUPwV78ZZf-sqWtX1VsTCa2_Q",
+    CONTEXT_KEY: "e595a5253b49f1a99",
+  },
+};
