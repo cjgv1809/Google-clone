675ba70ac0f11d0d290ae7a2efc21ad9770e8fe7
Carlos <carlos_gomes1809@hotmail.com>
2021-04-29 19:39:28 -0300

    final project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      animation: ["hover", "focus"],
+    },
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+};
